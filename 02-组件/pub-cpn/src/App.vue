70a87d688922f969c474cb387c63f70b160f8e30
it-reader <hnature@126.com>
2020-10-05 23:11:03 +0800

    Notice-Copmonent

--- 02-组件/pub-cpn/src/App.vue
+++ 02-组件/pub-cpn/src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Test></Test>
   </div>
 </template>
 
 <script>
+import Test from './components/Test'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Test
+  },
+  methods: {
+    
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- 02-组件/pub-cpn/src/main.js
+++ 02-组件/pub-cpn/src/main.js
@@ -2,14 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import create from './public/create'
 
 Vue.config.productionTip = false
+Vue.prototype.$create = create
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   components: { App },
   template: '<App/>'
 })
